87b3e9b48e715e5e3e73757cc706d8bddb7491ca
Thibesan Raveenderan <traveen2@uwo.ca>
2022-11-29 01:18:31 -0500

    Updated Codebase with more recent base version
    Updated with all updated files to be used for further development (merging)

--- server/routes/artists.js
+++ server/routes/artists.js
@@ -1,40 +1,40 @@
-const artist = require('../models/artist');
+const artist = require("../models/artist");
 
-const router = require('express').Router();
+const router = require("express").Router();
 
 //Test Route for establishing Postman
 router.get("/getAll/", async (req, res) => {
-    return res.json("getting all artists");
-})
+  return res.json("getting all artists");
+});
 
-//Artist Get One 
+//Artist Get One
 router.get("/getOne/:id", async (req, res) => {
-    const filter = {artist_id: req.params.id};
+  const filter = { artist_id: req.params.id };
 
-    const data = await artist.findOne(filter);
+  const data = await artist.findOne(filter);
 
-    if(data) {
-        return res.json(data);
-    } else {
-        return res.json('Could not be Found');
-    }
-})
+  if (data) {
+    return res.json(data);
+  } else {
+    return res.json("Could not be Found");
+  }
+});
 
 //Artist search by name
 router.get("/search/:id", async (req, res) => {
-    const searchRequest = req.params.id;
-    const filterArtist = 
-        {"artist_name": {$regex: searchRequest, $options: "i"}}
-    const select = {artist_id: 1, artist_name: 1, _id: 0}; //Filter resulting data for desired fields (ID and Name)
+  const searchRequest = req.params.id;
+  const filterArtist = {
+    artist_name: { $regex: searchRequest, $options: "i" },
+  };
+  const select = { artist_id: 1, artist_name: 1, _id: 0 }; //Filter resulting data for desired fields (ID and Name)
 
-    const data = 
-        await artist.find(filterArtist, select);
+  const data = await artist.find(filterArtist, select);
 
-    if(data){
-        return res.json(data);
-    } else{
-        return res.json('Could not be Found');
-    }
-})
+  if (data) {
+    return res.json(data);
+  } else {
+    return res.json("Could not be Found");
+  }
+});
 
 module.exports = router;
