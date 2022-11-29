bc8a898abcb5ee1a7d2dedb3966cf36e7a09e7e8
Thibesan Raveenderan <traveen2@uwo.ca>
2022-11-28 23:18:03 -0500

    Added Login Component
    Login Landing Page as component with necessary packages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,37 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { getAuth } from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { Home, Login } from "./components";
+import { app } from "./config/firebase.config";
 
 const App = () => {
+  const firebaseAuth = getAuth(app);
+  const navigate = useNavigate();
+
+  const [auth, setAuth] = useState(
+    false || window.localStorage.getItem("auth") === "true"
+  );
+
+  useEffect(() => {
+    //On Page Startup
+    firebaseAuth.onAuthStateChanged((userCredentials) => {
+      if (userCredentials) {
+        userCredentials.getIdToken().then((token) => {
+          console.log(token);
+        });
+      } else {
+        //If auth state false, redirect to login
+        setAuth(false);
+        window.localStorage.setItem("auth", "false");
+        navigate("/login");
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
       <Routes>
-        <Route path="/login/" element={<Login />} />
+        <Route path="/login/" element={<Login setAuth={setAuth} />} />
         <Route path="/*" element={<Home />} />
       </Routes>
     </div>
--- my-app/src/components/Login.jsx
+++ my-app/src/components/Login.jsx
@@ -1,9 +1,159 @@
-import React from 'react'
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import GoogleButton from 'react-google-button'
 
-const Login = () => {
+import { app } from '../config/firebase.config'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import {useNavigate} from 'react-router-dom'
+import { useEffect } from 'react';
+
+function Copyright(props) {
   return (
-    <div>Login</div>
-  )
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+        FullStackMusicPlayer
+      {' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default Login
+export default function SignInSide({setAuth}) {
+  const firebaseAuth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  const googleSignIn = async () => {
+    await signInWithPopup(firebaseAuth, provider)
+    .then((userCredentials) => {
+      if(userCredentials){ //If user credentials already exist, set auth true
+        setAuth(true);
+        window.localStorage.setItem("auth", "true");
+
+        firebaseAuth.onAuthStateChanged((userCredentials) => {
+          if (userCredentials) { //If exists, redirect to homepage
+            userCredentials.getIdToken().then((token) => {
+              console.log(token);
+            });            
+            navigate("/", {replace: true});
+          } else { //Redirect to login if auth state is false
+            setAuth(false);
+            navigate("/login");
+          }
+        });
+      }
+    })
+  }
+
+  useEffect(() => { //Redirect to homepage if auth state is true
+    if (window.localStorage.getItem("auth") === "true") {
+      navigate("/", {replace: true})
+    }
+  }, [])
+
+
+
+
+  return (
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(https://wallpaperaccess.com/full/427393.jpg)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 3 }}
+              >
+                Sign In
+              </Button>
+              <GoogleButton
+                onClick={googleSignIn}
+              />
+              <Grid container
+                sx={{ mt: 3, mb: 3 }}
+              >
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+  );
+}
