c1a6cefe994f39027bc6ffad0dcc00ea3ac70414
Thibesan Raveenderan <traveen2@uwo.ca>
2022-11-29 18:10:18 -0500

    Bug, Routing, Import Fixes
    Updating codebase to be pulled for further development

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -32,7 +32,7 @@ const App = () => {
     <div className="App">
       <Routes>
         <Route path="/login/" element={<Login setAuth={setAuth} />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/*" element={<Home />} />
       </Routes>
     </div>
   );
--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import { Button } from '@mui/material';
-import { Button } from '@mui/material';
 import React from 'react'
+import NavBar from './NavBar'
 
 const Home = () => {
     const testFetch = () => {
